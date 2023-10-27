6fc284371e1ab7d777c8a20194e6b52bec538aa0
JingYuan Wu <jw26u22@soton.ac.uk>
2023-10-27 16:16:58 +0100

    slides

--- M4/multiplier/sequencer.sv
+++ M4/multiplier/sequencer.sv
@@ -7,25 +7,60 @@
 //            : Outline code for sequencer
 //            :
 // Limitations: None
-//            : 
+//            :
 // System     : SystemVerilog IEEE 1800-2005
 //            :
-// Author     : 
+// Author     :
 //            : School of Electronics and Computer Science
 //            : University of Southampton
 //            : Southampton SO17 1BJ, UK
-//            : 
+//            :
 //
-// Revision   : Version 1.0 
+// Revision   : Version 1.0
 /////////////////////////////////////////////////////////////////////
 
 module sequencer (input logic start, clock, Q0, n_rst,
  output logic add, shift, ready, reset);
- 
-// Your code goes here
-  
+
+ enum {IDLE, ADDING, SHIFTING, STOPPED} present_state, next_state;
+
+ always_ff @(posedge clock, negedge n_rst)
+    begin: SEQ
+     if(!n_rst)
+        present_state <= IDLE;
+    else
+        present_state <= next_state;
+    end
+
+ always_comb
+    begin: COM
+    add = '0;
+    shift = '0;
+    ready = '0;
+    reset = '0;
+
+    unique_case (present_state)
+        IDLE: begin
+
+        end
+
+        ADDING: begin
+
+        end
+
+        SHIFTING: begin
+
+        end
+
+        STOPPED: begin
+
+        end
+    endcase
+    end
+
+//Last edited:20231027
+
 endmodule
 
-      
-              
-             
+
+

bd048a1b5469506bbdcf7818057ef8cc3935cd5e
JingYuan Wu <jw26u22@soton.ac.uk>
2023-10-29 18:33:58 +0000

    2023/10/29 周日

---
+++ S1/s1prep.m
@@ -0,0 +1,43 @@
+% system
+
+
+% zeta = 0
+servo1 = tf(1,[1 0 1 0]);
+% zeta = 0.5
+servo2 = tf(1,[1 1 1 0]);
+% zeta = 1
+servo3 = tf(1,[1 2 1 0]);
+% zeta = 2
+servo4 = tf(1,[1 4 1 0]);
+
+
+t = 0:0.01:20;
+
+[y1,t] = step(servo1,t);
+
+[y2,t] = step(servo2,t);
+
+[y3,t] = step(servo3,t);
+
+[y4,t] = step(servo4,t);
+
+
+subplot(2,2,1);
+plot(t,y1);
+title('step responce zeta = 0');
+grid on;
+
+subplot(2,2,2);
+plot(t,y2);
+title('step responce zeta = 0.5');
+grid on;
+
+subplot(2,2,3);
+plot(t,y3);
+title('step responce zeta = 1');
+grid on;
+
+subplot(2,2,4);
+plot(t,y4);
+title('step responce zeta = 2');
+grid on;
